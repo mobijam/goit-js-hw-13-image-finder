6a25a218562d80e09ac8f40bb87af2ca190d4ee0
Viktoriia Strelnikova <v.strelnikova91@gmail.com>
2020-12-06 15:52:47 +0200

    rewrite

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,18 +1,19 @@
 const URL = 'https://pixabay.com/api/';
 const KEY = "19414082-e0889b705c9c0a911a7721b73";
 
- /*=============Функция выполняющая фетч к АПИ==============*/
 export default {
-    glSearchQuery: '',
-    page: 1,
-    fetchImageByQuery() {
-    const url = `${URL}&q=${this.query}&page=${this.page}&per_page=12&key=${KEY}`;
-        return fetch(url)
-            .then(res => res.json())
-            .then(({ hits }) => { 
-                this.page += 1;
-                return hits;
-            });
+       constructor() {
+        this.searchQuery = '';
+        this.page = 1;
+    },
+    
+        fetchImageByQuery() {
+        
+    return fetch(`${URL}/?image_type=photo&orientation=horizontal&q=${this.searchQuery}&page=${this.page}&per_page=28&key=${KEY}` )
+    .then(pictures => pictures.json()).then(({ hits }) => {
+        this.page += 1;
+        return hits
+    })       
     },
     
     resetPage() { 
